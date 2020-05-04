201d7187e9045751831e9ca397dc372f731ac1bc
fjhansen <58237856+fjhansen@users.noreply.github.com>
2020-05-04 11:12:44 -0700

    complete

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const express = require('express')
 //Create server
 const server = express();
 
+server.use(express.json()); // teaches express to use JSON from the body
+
   // return an array of lessons (id, name)
   // this is in global scope, so other functions can use it.
   let lessons = [
@@ -33,6 +35,15 @@ server.post('/api/lessons', function(req, res) {
   lessons.push(lessonInformation);
 
   res.status(201).json(lessonInformation);
+})
+
+// everything that comes out of URL is a string. so remember to use Number constructor or use != instead of !==
+server.delete("/api/lessons/:id", function(req, res) {
+  const id = Number(req.params.id);
+
+  // find the lesson on the array and remove it
+  lessons = lessons.filter(lesson => lesson.id !== id);
+  res.status(200).json(lessons);
 })
 
 server.listen(8000, () => console.log("\n==API IS UP==\n"))
